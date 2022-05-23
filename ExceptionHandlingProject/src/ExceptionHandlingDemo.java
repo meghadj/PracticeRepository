b276b5291beb1b769f3962491960b6203a337508
Deepak <Deepak@DESKTOP-KJTMH01>
2022-05-23 13:24:39 +0530

    "i am uploading my files to repository"

---
+++ ExceptionHandlingProject/src/ExceptionHandlingDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ExceptionHandlingDemo {
+
+	public static void main(String[] args) throws ArithmeticException {
+		    int x, y, result=0;
+		    Scanner sc=new Scanner(System.in);
+		    System.out.println("Enter two numbers");
+		    x=sc.nextInt();
+		    y=sc.nextInt();
+		    try {
+		    result=x/y;
+		    }
+		    catch (ArithmeticException e) {
+		        System.out.println("Cannot divide by zero");
+		    }
+		    finally {
+		    System.out.println("Result is "+result);
+		    }
+
+	}
+
+}
